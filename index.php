e5cdce9ad004e92c63bc78dac5fc6e90a7f662dc
The-Maven <anthonykwawu@gmail.com>
2015-05-10 20:14:28 +0100

    Pushed local development code to github server after exporting files from code.google.com. Modified professor class, add and edit course functions, the functions file, created new wiki, reconfigured the AJAX response page.

--- index.php
+++ index.php
@@ -1,62 +1,80 @@
-<html>
-	<head>
-		<title>Index</title>
-		<link rel="stylesheet" href="css/style.css">
-		<link rel="stylesheet" href=font-awesome-4.3.0/css/font-awesome.min.css>
-		<script>
-			
-		</script>
-	</head>
-	<body>
-		<table id="maintable">
-			<tr>
-				<td colspan="2" id="pageheader" <i class="fa fa-3x fa-institution"></iclass></i>>
-					Application Header
-				</td>
-			</tr>
-			<tr>
-				<td id="mainnav">
-					<div class="nav"><i class="fa fa-3x fa-book" ></i><br>Courses</div>
-					<div class="nav"><i class="fa fa-3x fa-graduation-cap"></i><br>Professors</div>
-					<div class="nav"><i class="fa fa-3x fa-institution"></i><br>Faculty</div>
-<!--					<div class="nav">Description</div>-->
-				</td>
-				<td id="content">
-					<div id="divPageMenu">
-						<span  >Add</span>
-						<span  >Edit</span>
-						<span  >Delete</span>
-						<input type="text" id="txtSearch" />
-						<span >search</span>
-					</div>
-					<div id="divStatus" class="status">
-						status message
-					</div>
-					<div id="divContent">
-						Content space
-						<span class="clickspot">click here </span>
-						<table id="tableExample" class="reportTable" width="100%">
-							<tr class="header">
-								<td>column1</td>
-								<td>column2</td>
-								<td>column3</td>
-								<td>column4</td>
-							</tr>
-							<tr class="row1">
-								<td>data example</td>
-								<td>123</td>
-								<td>01/01/2014</td>
-								<td>data</td>
-							</tr>
-							<tr class="row2">
-								<td>data example</td>
-								<td>123</td>
-								<td>01/01/2014</td>
-								<td>data</td>
-							</tr>
-					</div>
-				</td>
-			</tr>
-		</table>
-	</body>
-</html>	
+<?php
+require_once 'header.php';
+  $error = $user = $pass = "";
+
+  if (isset($_POST['user']))
+  {
+    $user = sanitizeString($_POST['user']);
+    $pass = sanitizeString($_POST['pass']);
+    
+    if ($user == "" || $pass == "")
+        $error = "Not all fields were entered<br>";
+    else
+    {
+      $result = queryMySQL("SELECT username,password FROM Credentials
+        WHERE username='$user' AND password='$pass'");
+
+      if ($result->num_rows == 0)
+      {
+        $error = "<span class='error'>Username/Password
+                  invalid</span><br><br>";
+      }
+      else
+      {
+        $_SESSION['user'] = $user;
+        $_SESSION['pass'] = $pass;
+        die("You are now logged in. Please <a href='home.php?view=$user'>" .
+            "click here</a> to continue.<br><br>");
+      }
+    }
+  }
+
+echo <<<_END
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Courseware™</title>
+    <!-- Bootstrap -->
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="css/moveme.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.min.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+    <body>
+<div class="container outerlogin">
+	<h1>User login <span class='glyphicon glyphicon-education' aria-hidden='true'></span></h1>
+	<hr/>
+    <div class="row">
+        <div class="col-sm-4 col-offset-8 login">
+    <form method="post" action="index.php">$error
+    	<div class="form-group">
+    		<label for="user">Username</label>
+			<input type="type" class="form-control" name="user" value="$user" placeholder="Enter username" required>
+    	</div>
+
+    	<div class="form-group">
+    		<label for="passwordField">Password</label>
+			<input type="password" class="form-control" name="pass" value="$pass" placeholder="Enter password" required>
+    	</div>
+
+    	<button type="submit" class="btn btn-success" value="Login">Login</button>
+    </form>
+    </div>
+    </div>
+</div>
+
+</body>
+</html>
+_END;
+?>
